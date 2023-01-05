47c30e5ff36c46c1503d72ce58395c170f9b1cbd
Andrés Ignacio Jara <andyjara.guitar.7@gmail.com>
2023-01-05 00:13:15 -0300

    Se acomoda el form y algunas validaciones

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,38 +1,63 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   LoginScreen({Key? key}) : super(key: key);
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   bool _oscureText = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      decoration: const BoxDecoration(color: Colors.red),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              obscureText: _oscureText,
-              decoration: InputDecoration(
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    _oscureText = !_oscureText;
-                  },
-                  icon: Icon(
-                      _oscureText ? Icons.visibility : Icons.visibility_off),
+        body: Form(
+      autovalidateMode: AutovalidateMode.always,
+      key: GlobalKey<FormState>(),
+      child: Container(
+        decoration: const BoxDecoration(color: Colors.red),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                  decoration: const InputDecoration(label: Text('Email')),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    if (!RegExp(
+                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                        .hasMatch(value)) {
+                      return 'Please enter a valid email';
+                    }
+                    return null;
+                  }),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                obscureText: _oscureText,
+                decoration: InputDecoration(
+                  label: const Text('Password'),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      _oscureText = !_oscureText;
+                      setState(() {});
+                    },
+                    icon: Icon(
+                        _oscureText ? Icons.visibility : Icons.visibility_off),
+                  ),
                 ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     ));
   }
